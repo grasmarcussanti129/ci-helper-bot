fdceb660940717ef3227639b370dbf0d286d3b7f
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-10-06 13:10:03 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('CI Helper Bot is running!');
+});
+
+app.listen(port, () => {
+    console.log(`CI Helper Bot listening at http://localhost:${port}`);
+});
